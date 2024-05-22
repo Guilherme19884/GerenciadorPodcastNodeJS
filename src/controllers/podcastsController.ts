0e0ce1692c6ace5ce748cc929a71168378110de2
Guilherme <guilherme1984.araujo@gmail.com>
2024-05-22 18:59:01 -0300

    impletado service e melhoria no controller

--- src/controllers/podcastsController.ts
+++ src/controllers/podcastsController.ts
@@ -1,9 +1,12 @@
 import { IncomingMessage, ServerResponse } from 'http'
 
+import { serviceListEpisodes } from '../services/lisEpisodesService'
+
 
 export const getListEpisodes  = async ( request: IncomingMessage, response: ServerResponse) =>{
+
+    const content = serviceListEpisodes()
+
     response.writeHead(200,{'Contet-Type': 'application/jose'})
-    response.end(JSON.stringify({
-        mensagem: "retornando mensagem",
-    }))
+    response.end(JSON.stringify({content}))
 }
---
+++ src/services/lisEpisodesService.ts
@@ -0,0 +1,23 @@
+
+
+
+export const serviceListEpisodes = ()=> {
+    const data = [
+        {
+        podcastName: "flow",
+        episode: "CBUM - flow #319",
+        cover: "https://i.ytimg.com/vi/pQSuQmUfS30/maxresdefault.jpg",
+        videoId: "pQSuQmUfS30",
+        link: "https://www.youtube.com/watch?v=pQSuQmUfS30&t=4s",
+        categories: ["body builder", "esporte", "saúde"],
+        },
+        {
+        podcastName: "flow",
+        episode: "RUBENS BARRICHELLO -Flow #339",
+        cover: "https://i.ytimg.com/vi/4KDGTdiOV4I/maxresdefault.jpg",
+        link: "https://www.youtube.com/watch?v=4KDGTdiOV4I",
+        videoId: "4KDGTdiOV4I",
+        categories: ["Esporte", "Corrida"],
+        }
+    ]   
+}
